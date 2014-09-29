9d96ca9aef9179158051134b985ecad37bb6fa7c
precise-asad <asad@precisetech.com.pk>
2014-09-29 17:09:20 +0500

    po details attribute changes

--- POS.Domain.Common/PO_DetailsClass.cs
+++ POS.Domain.Common/PO_DetailsClass.cs
@@ -3,26 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using POS.Domain.Base;
 
 namespace POS.Domain.Common
 {
-   public class PO_DetailsClass
+   public class PO_DetailsClass: BaseDomain
     {
         public int PO_Number { get; set; }
         public string ItemNum { get; set; }
         public int LineNum { get; set; }
-        public float Quan_Ordered { get; set; }
-        public decimal CostPer { get; set; }
-        public float Quan_Received { get; set; }
+        public decimal Quan_Ordered { get; set; }
+        public double CostPer { get; set; }
+        public decimal Quan_Received { get; set; }
         public string Vendor_Part_Number { get; set; }
         public int CasePack { get; set; }
         public string Store_ID { get; set; }
         public string destStore_ID { get; set; }
-        public float Current_Batch_Quan { get; set; }
-        public float Quan_Damaged { get; set; }
+        public double Current_Batch_Quan { get; set; }
+        public decimal Quan_Damaged { get; set; }
         public string Reason { get; set; }
-        public float NumberPerCase { get; set; }
+        public decimal NumberPerCase { get; set; }
         public int OverrideCommission { get; set; }
-        public float Quan_OutofDate { get; set; }
+        public double Quan_OutofDate { get; set; }
     }
 }
